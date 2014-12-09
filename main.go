d54139ccc398671ce5466894b8a913607cc18908
Peter Buchmann <peter.buchmann@web.de>
2014-12-09 17:35:36 +0100

    limits of plastered amorphs computed

--- main.go
+++ main.go
@@ -8,6 +8,11 @@ import (
 var spf func(format string, a ...interface{}) string = fmt.Sprintf
 var pf func(format string, a ...interface{}) (int, error) = fmt.Printf
 
+const MaxUint = ^uint(0)
+const MinUint = 0
+const MaxInt = int(MaxUint >> 1)
+const MinInt = -MaxInt - 1
+
 func main() {
 	pf("start listening on 4001\n")
 	http.ListenAndServe("localhost:4001", nil) // dont put into http server init() - it blocks the other init() funcs
--- p7 - matrix.go
+++ p7 - matrix.go
@@ -7,8 +7,6 @@ import (
 	"github.com/pbberlin/tools/util"
 )
 
-// var Matrix [][]*Amorph
-
 type Slot struct {
 	A         *Amorph
 	IsLeftTop bool
@@ -17,61 +15,34 @@ type Slot struct {
 type Matrix [][]Slot
 
 type Layout struct {
-	M          Matrix
-	WestLimes  int
-	WestWall   int
-	SouthLimes int
-	SouthWall  int
-	NorthLimes int
-	NorthWall  int
-	EastLimes  int
-	EastWall   int
+	M                        Matrix
+	West, South, East, North int
 }
 
 var L1 Layout
 
-func matrixRaw(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-
-	// s1 := util.IndentedDump(AmorphsRandom)
-	// io.WriteString(w, *s1)
-
-	io.WriteString(w, "showing current (layout) matrix \n\n")
-	s2 := util.IndentedDump(L1.M)
-	io.WriteString(w, *s2)
-}
-
-func init() {
-	L1.Init()
-	L1.Seed()
-	// L1.Fill(&AmorphsRandom[0], 7, 8)
-	// L1.Fill(&AmorphsRandom[1], 7, 12)
-	L1.Delimit()
-
-	// L1.M[4][5].IsLeftTop = true
-}
 func (l *Layout) Init() {
 	newM := make([][]Slot, gridRows) // top-left; rows-cols; x=> rows, y=> cols, *not* Ordinate/Abszisse convention
 	for i := 0; i < len(newM); i++ {
 		newM[i] = make([]Slot, gridCols)
 	}
 	l.M = newM
+
 }
 
+// Plaster some randomly generated amorphs onto the layout
 func (l *Layout) Seed() {
-
 	seeds := 3
 	lastColPos := 5
 
 	for i := 0; i < seeds; i++ {
-		L1.Fill(&AmorphsRandom[i], 5, lastColPos)
+		L1.Plaster(&AmorphsRandom[i], 5, lastColPos)
 		lastColPos += AmorphsRandom[i].Ncols + 1
 	}
-
 }
 
-func (l *Layout) Fill(a *Amorph, top, left int) {
-
+// Paste an amorph onto a matrix
+func (l *Layout) Plaster(a *Amorph, top, left int) {
 	for i := top; i < top+a.Nrows; i++ {
 		for j := left; j < left+a.Ncols; j++ {
 			if i < len(l.M) && j < len(l.M[0]) {
@@ -82,15 +53,52 @@ func (l *Layout) Fill(a *Amorph, top, left int) {
 		}
 	}
 	l.M[top][left].IsLeftTop = true
+}
+
+// Redraw borders
+func (l *Layout) Delimit() {
+
+	w := MaxInt
+	e := 0
+	n := MaxInt
+	s := 0
+	for row := 0; row < len(l.M); row++ {
+		for col := 0; col < len(l.M[row]); col++ {
+			if l.M[row][col].A != nil {
+				if col < w {
+					w = col
+				}
+				if col > e {
+					e = col
+				}
+
+				if row < n {
+					n = row
+				}
+				if row > s {
+					s = row
+				}
+			}
+		}
+	}
+	l.West = w
+	l.East = e + 1
+	l.North = n
+	l.South = s + 1
 
 }
 
-func (l *Layout) Delimit() {
-	if l.M == nil || l.M[0] == nil {
-		panic("Layout struct must be initialized before limiting it")
-	}
-	m := l.M
-	cy := len(m) / 2
-	cx := len(m[0]) / 2
-	pf("heart of the layout %v %v\n", cx, cy)
+func matrixRaw(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	io.WriteString(w, "showing current (layout) matrix \n\n")
+	s2 := util.IndentedDump(L1)
+	io.WriteString(w, *s2)
+}
+
+func init() {
+	L1.Init()
+	L1.Seed()
+	// L1.Plaster(&AmorphsRandom[0], 7, 8)
+	// L1.Plaster(&AmorphsRandom[1], 7, 12)
+	L1.Delimit()
 }
